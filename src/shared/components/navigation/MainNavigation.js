b0544f0d2aced463a82a608c360d42f940fd00ba
DeepaThangaraju <deepat2405@gmail.com>
2022-03-25 00:37:15 +0530

    title

--- src/shared/components/navigation/MainNavigation.js
+++ src/shared/components/navigation/MainNavigation.js
@@ -31,7 +31,7 @@ const MainNavigation = props => {
            <span/>
            <span/>
         </button>
-        <h1 className='main-navigation__title'><Link to="/">Place Finder</Link></h1>
+        <h1 className='main-navigation__title'><Link to="/">Share Experience</Link></h1>
         <nav className='main-navigation__header-nav'>
            <NavLinks/>
         </nav>
