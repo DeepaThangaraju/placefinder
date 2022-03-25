a7fe65f66e46c7c75c5e34761a878c32ce1124df
DeepaThangaraju <deepat2405@gmail.com>
2022-03-25 11:50:31 +0530

    api

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -11,7 +11,7 @@ const UserItem = props => {
       <Card className="user-item__content">
         <Link to={`/${props.id}/places`}>
           <div className="user-item__image">
-            <Avatar image={`https://deepaplacefinder.herokuapp.com/${props.image}`} alt={props.name} />
+            <Avatar image={`http://localhost:9000/${props.image}`} alt={props.name} />
             
           </div>
           <div className="user-item__info">
